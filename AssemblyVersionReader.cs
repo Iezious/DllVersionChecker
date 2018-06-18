d66d852de4a937e931a5fc95fdb55aeac0d87cfe
Sergey Kilimnik <sergey-k@sbtech.com>
2018-06-18 18:42:17 +0300

    Working version

---
+++ AssemblyVersionReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace DLLVersionCheck
+{
+    public class AssemblyVersionReader : MarshalByRefObject
+    {
+        public  string[][] ReadFolder(string folder)
+        {
+            Console.WriteLine($"Execution for {folder}");
+            
+            var res = new List<string[]>();
+
+            foreach (var filename in Directory.GetFiles(folder))
+            {
+                switch (Path.GetExtension(filename))
+                {
+                    case ".dll":
+                    case ".exe":
+                        try
+                        {
+                            var assembly = Assembly.LoadFrom(filename);
+                            var name = assembly.GetName();
+
+                            res.Add(new[] {name.Name, name.Version.ToString()});
+                                
+                            Console.Write("[");
+                            WriteInColor(" OK ", ConsoleColor.DarkGreen);
+                            Console.WriteLine($"] {assembly.GetName().Name}");
+                        }
+                        catch
+                        {
+                            Console.Write("[");
+                            WriteInColor("SKIP", ConsoleColor.Yellow);
+                            Console.WriteLine($"] {filename}");
+                        }
+                            
+                        break;
+                }
+            }
+
+            Console.WriteLine();
+            return res.ToArray();
+        }
+        
+        
+        static void WriteInColor(string text, ConsoleColor color)
+        {
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DLLVersionCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var domains = new List<string[][]>();
+            
+            foreach (var path in args)
+            {
+                var appDomain = AppDomain.CreateDomain(path);
+                
+                dynamic reader = appDomain.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().FullName, "DLLVersionCheck.AssemblyVersionReader", true, BindingFlags.Default,
+                    null, null, null, null);
+
+                var data = reader.ReadFolder(path) as string[][];
+                
+                domains.Add(data);
+            }
+
+            var parsed = domains.Select(FetchNames).ToArray();
+            var names = parsed.SelectMany(di => di.Keys).Distinct().OrderBy(v => v).ToArray();
+            
+            Console.WriteLine("=========================================================================================================================");
+            foreach (var name in names)
+            {
+                Console.Write($"{name, -60}");
+                string baseVersion = null;
+
+                foreach (var collection in parsed)
+                {
+                    if (collection.TryGetValue(name, out var version))
+                    {
+                        if (baseVersion == null)
+                            baseVersion = version;
+
+                        if (version != baseVersion)
+                        {
+                            WriteInColor($"{version,20}", ConsoleColor.Red);
+                        }
+                        else
+                        {
+                            Console.Write($"{version,20}");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("{0,20}", "NONE");
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+        }
+
+        private static Dictionary<string, string> FetchNames(string[][] domain)
+        {
+            return domain.ToDictionary(pair => pair[0], pair => pair[1]);
+        }
+
+        static void WriteInColor(string text, ConsoleColor color)
+        {
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = c;
+        }
+
+
+    }
+}
